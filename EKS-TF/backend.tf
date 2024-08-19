f3c113cb227111b9d781a1f98540378e8dfce079
vijaybhaskar22 <vijaybhaskar.0122@gmail.com>
2024-08-19 15:45:07 +0530

    backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "mariobucket22" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "ap-south-2"
   }
 }
